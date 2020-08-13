f635a24af41adbb6f492192e865ace922b5f18bb
dlugi57 <dlugoszpeter@gmail.com>
2020-08-14 00:24:05 +0200

    RELEASE redirections of url

---
+++ src/main/java/com/openclassrooms/watchlist/CustomErrorController.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.watchlist;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @GetMapping("/error")
+    public ModelAndView handleError(HttpServletResponse response){
+
+        int code = response.getStatus();
+        System.out.println("Error with code: " + code + " happened");
+        if(code == 404 ){
+            return new ModelAndView("404");
+        }else{
+            return new ModelAndView("error");
+        }
+
+
+    }
+}
